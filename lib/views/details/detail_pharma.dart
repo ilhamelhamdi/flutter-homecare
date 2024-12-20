0054fe9183a0469da799a56cfa5f9c15c463df64
Chimera-pkg <adryanmagatsu88@gmail.com>
2024-12-20 16:21:25 +0700

    detailing  UI : add an Issue details

--- lib/views/details/detail_pharma.dart
+++ lib/views/details/detail_pharma.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '/views/search/search_pharma.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_homecare/widgets/image_preview.dart';
 
 class DetailPersonalPage extends StatelessWidget {
   @override
@@ -111,110 +113,46 @@ class _AddConcernPageState extends State<AddConcernPage> {
               ),
             ),
             SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Issue Title',
-                border: OutlineInputBorder(),
+            SizedBox(
+              width: 338,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Issue Title',
+                  hintStyle: TextStyle(color: Color(0xFFD0D0D0)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Please Enter Question',
-                border: OutlineInputBorder(),
+            SizedBox(height: 10.0),
+            Container(
+              width: 338,
+              height: 129,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText:
+                      'Please enter questions, concerns, relevant symptoms related to your case along with related keywords.',
+                  hintStyle: TextStyle(color: Color(0xFFD0D0D0)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 10), // Adjust the vertical padding as needed
+                ),
+                maxLines: null,
+                expands: true,
               ),
             ),
-            SizedBox(height: 20),
-            Text(
-              'Image less than 100MB',
-              style: TextStyle(fontSize: 12),
-            ),
-            Row(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.grey.shade200,
-                  child: Center(
-                    child: Text('Preview'),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Choose File',
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Add your choose file logic here
-                      },
-                      child: Text('Choose File'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.grey.shade200,
-                  child: Center(
-                    child: Text('Preview'),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Choose File',
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Add your choose file logic here
-                      },
-                      child: Text('Choose File'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.grey.shade200,
-                  child: Center(
-                    child: Text('Preview'),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Choose File',
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Add your choose file logic here
-                      },
-                      child: Text('Choose File'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            SizedBox(height: 20.0),
+            ImagePreview(),
+            SizedBox(height: 20.0),
+            ImagePreview(),
+            SizedBox(height: 20.0),
+            ImagePreview(),
+            SizedBox(height: 20.0),
             Spacer(),
             SizedBox(
               width: 352,
@@ -308,32 +246,48 @@ class AddSummaryPage extends StatelessWidget {
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AddConcernPage()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                      // primary: Colors.transparent,
-                      // onPrimary: Color(0xFF35C5CF),
-                      ),
-                  child: Text('Add an Issue'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AddIssuePage()),
-                    );
-                    // Add your next step logic here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    // primary: Color(0xFF35C5CF),
-                    backgroundColor: Color(0xFF35C5CF),
+                SizedBox(
+                  width: 352,
+                  height: 58,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AddConcernPage(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF35C5CF),
+                    ),
+                    child: Text(
+                      'Add an Issue',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                  child: Text('Next'),
+                ),
+                SizedBox(height: 10.0),
+                SizedBox(
+                  width: 352,
+                  height: 58,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AddIssuePage(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF35C5CF),
+                    ),
+                    child: Text(
+                      'Next',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -365,8 +319,8 @@ class _AddIssuePageState extends State<AddIssuePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Your mobility status',
-              style: TextStyle(fontSize: 16),
+              'Select your mobility status',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
             Column(
               children: [
@@ -379,6 +333,7 @@ class _AddIssuePageState extends State<AddIssuePage> {
                       _mobilityStatus = value!;
                     });
                   },
+                  activeColor: Color(0xFF35C5CF),
                 ),
                 RadioListTile<String>(
                   title: const Text('Wheelchair bound'),
@@ -389,6 +344,7 @@ class _AddIssuePageState extends State<AddIssuePage> {
                       _mobilityStatus = value!;
                     });
                   },
+                  activeColor: Color(0xFF35C5CF),
                 ),
                 RadioListTile<String>(
                   title: const Text('Ambulatory'),
@@ -399,6 +355,7 @@ class _AddIssuePageState extends State<AddIssuePage> {
                       _mobilityStatus = value!;
                     });
                   },
+                  activeColor: Color(0xFF35C5CF),
                 ),
                 RadioListTile<String>(
                   title: const Text('Independent'),
@@ -409,33 +366,54 @@ class _AddIssuePageState extends State<AddIssuePage> {
                       _mobilityStatus = value!;
                     });
                   },
+                  activeColor: Color(0xFF35C5CF),
                 ),
               ],
             ),
             SizedBox(height: 20),
             Text(
               'Select a related health record',
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            DropdownButton<String>(
-              value: _selectedStatus,
-              items: <String>[
-                'Select Status',
-                'Status 1',
-                'Status 2',
-                'Status 3'
-              ].map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  _selectedStatus = newValue!;
-                });
-              },
+            SizedBox(height: 10.0),
+            Container(
+              width: 362,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  value: _selectedStatus,
+                  items: <String>[
+                    'Select Status',
+                    'Status 1',
+                    'Status 2',
+                    'Status 3'
+                  ].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedStatus = newValue!;
+                    });
+                  },
+                  isExpanded: true,
+                  icon: Icon(Icons.arrow_drop_down),
+                  iconSize: 24,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                  dropdownColor: Colors.white,
+                ),
+              ),
             ),
+            Spacer(),
             ElevatedButton(
               onPressed: () {
                 // Add your add logic here
---
+++ lib/widgets/image_preview.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class ImagePreview extends StatefulWidget {
+  @override
+  _ImagePreviewState createState() => _ImagePreviewState();
+}
+
+class _ImagePreviewState extends State<ImagePreview> {
+  File? _image;
+
+  Future<void> _chooseImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.grey.shade200,
+          child: _image == null
+              ? Center(child: Image.asset('assets/icons/ic_preview.png'))
+              : Image.file(_image!, fit: BoxFit.cover),
+        ),
+        SizedBox(width: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Image less than 100MB',
+              style: TextStyle(fontSize: 12),
+            ),
+            SizedBox(height: 10),
+            OutlinedButton(
+              onPressed: _chooseImage,
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Color(0xFF10B981)),
+              ),
+              child: Text(
+                'Choose File',
+                style: TextStyle(color: Color(0xFF10B981)),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
